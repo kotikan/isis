98af34921dd179a164d7d46333ba0f2f836a268d
Charles Harley <charles.harley@googlemail.com>
2014-04-23 18:30:10 +0100

    Updated the Classic Yasir! plugin to respond to any speaker at random
    Kept the plugin name as a reminder of its origin.

--- plugins/classicyasir/classicyasir.rb
+++ plugins/classicyasir/classicyasir.rb
@@ -2,18 +2,35 @@
 
 class Classicyasir < Isis::Plugin::Base
 
-  RESPONSES = ['Classic Yasir!', 'CLASSIC YASIR', 'Classsssssic Yasir']
-
   def respond_to_message?(message)
-    if /yasir/i =~ message.content + message.speaker
-      Random.new().rand(1..10) == 5
-    else
-      false
-    end
+    @speaker_first_name = speaker_first_name(message)
+
+    Random.new().rand(1..10) == 5
   end
 
   def response_text
-    RESPONSES[Random.new().rand(0..(RESPONSES.length - 1))]
+    # I could do this using good OO techniques but it isn't worth it yet
+    # for this little delightful plugin. Sorry Uncle Bob!
+    response_type = 'response_type_' + Random.new().rand(1..3).to_s
+    self.send(response_type, @speaker_first_name)
+  end
+
+  private
+
+  def speaker_first_name(message)
+    message.speaker.split[0]
+  end
+
+  def response_type_1(speaker_first_name)
+    "Classic #{speaker_first_name}!"
+  end
+
+  def response_type_2(speaker_first_name)
+    "CLASSIC #{speaker_first_name.upcase}"
+  end
+
+  def response_type_3(speaker_first_name)
+    "Classsssssic #{speaker_first_name}"
   end
 
 end
