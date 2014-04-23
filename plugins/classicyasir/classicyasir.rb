85740468ff41678a65d7aecd309ae19d88903657
Charles Harley <charles.harley@googlemail.com>
2014-04-23 14:44:11 +0100

    Classic Yasir! plugin responds at random
    Also deleted the tests as they currently don't make sense with random responses.

--- plugins/classicyasir/classicyasir.rb
+++ plugins/classicyasir/classicyasir.rb
@@ -2,16 +2,18 @@
 
 class Classicyasir < Isis::Plugin::Base
 
+  RESPONSES = ['Classic Yasir!', 'CLASSIC YASIR', 'Classsssssic Yasir']
+
   def respond_to_message?(message)
     if /yasir/i =~ message.content + message.speaker
-      true
+      Random.new().rand(1..10) == 5
     else
       false
     end
   end
 
   def response_text
-    'Classic Yasir!'
+    RESPONSES[Random.new().rand(0..(RESPONSES.length - 1))]
   end
 
 end
