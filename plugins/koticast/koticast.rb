375a635841f45ec0628f87d51843214388302af6
Jamie Toolin <jamie@kotikan.com>
2014-04-24 12:51:47 +0100

    Move forecast.io API key into a config.yml file and include a link to the full forecast.

--- plugins/koticast/koticast.rb
+++ plugins/koticast/koticast.rb
@@ -1,6 +1,7 @@
 # Returns the current weather forecast for Kotikan HQ
 
 require 'forecast_io'
+require 'yaml'
 
 class Koticast < Isis::Plugin::Base 
 
@@ -11,20 +12,26 @@ class Koticast < Isis::Plugin::Base
 
   def response_html
     forecast = koticast
-    %Q(<strong>Weather at Kotikan HQ: </strong>#{forecast[:minutely]} #{forecast[:hourly]})
+    %Q(<strong>Weather at Kotikan: </strong>#{forecast[:minutely]} #{forecast[:hourly]} <a href="#{forecast[:url]}">Full report...</a>)
   end
 
   def response_text
     forecast = koticast
-    'Weather at Kotikan HQ:' + forecast[:minutely] + ' ' + forecast[:hourly]
+    'Weather at Kotikan:' + forecast[:minutely] + ' ' + forecast[:hourly]
   end
 
   private
 
   def koticast
-    ForecastIO.api_key = 'API_KEY'
-    forecast = ForecastIO.forecast(55.946802, -3.201294)
-    { minutely: forecast.minutely.summary, hourly: forecast.hourly.summary }
+    ForecastIO.api_key = forecast_api_key
+    lat, lon = 55.946802, -3.201294
+    forecast = ForecastIO.forecast(lat, lon)
+    { minutely: forecast.minutely.summary, hourly: forecast.hourly.summary url: %Q(http://forecast.io/#/f/#{lat},#{lon}) }
+  end
+  
+  def forecast_api_key
+    config = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
+    config["api_key"]
   end
 
 end
